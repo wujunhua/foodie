30f2132881afaa81c00d81cfbd0cd079807aa368
Raymond Zhu <junhao.raymond.zhu@gmail.com>
2017-05-01 16:44:34 -0400

    Updated model adding image_url field.

--- foodie/models/menu.py
+++ foodie/models/menu.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Menu(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
     price = models.IntegerField()
     rating = models.FloatField(default=None)
     num_ratings = models.IntegerField(default=0)
+    image_url = models.TextField(max_length=100, default="")
 
     def update_rating(self, rating):
         self.rating =  (self.rating * self.num_ratings + rating) / (self.num_ratings + 1)
