00f6bf631cfd0f88a821c10c40535a9874c2b7ae
Raymond Zhu <junhao.raymond.zhu@gmail.com>
2017-05-01 21:18:38 -0400

    Added login view

--- foodie/urls.py
+++ foodie/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', auth_views.login,  {'template_name': 'login.html', 'authentication_form': LoginForm, 'extra_context':{'next' : '/'}}, name='login'),
 #   url(r'^', include('main.urls')),
     url(r'^admin/', admin.site.urls),
 ]
