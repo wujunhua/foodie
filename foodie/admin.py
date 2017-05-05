078aaf50ac66320809dbb4cf34fb001a5f5399bf
mike <mike@nighttype.com>
2017-05-05 04:00:27 -0400

    added Order class & admin view, put more details/pics in menu.json =)

--- foodie/admin.py
+++ foodie/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import UserProfile
 from .models import Menu
+from foodie.models import Order
 
 class UserProfileAdminModel(admin.ModelAdmin):
     model = UserProfile
@@ -19,9 +20,19 @@ class MenuAdminModel(admin.ModelAdmin):
     list_display = ["name", "short_description", "price", "rating", "created_by"]
     list_display_links = ["name", "created_by"]
     list_filter = ["name", "rating", "created_by"]
-
     search_fields = ["name", "created_by"]
     class Meta:
         model = Menu
 
 admin.site.register(Menu, MenuAdminModel)
+
+
+class OrderAdminModel(admin.ModelAdmin):
+    list_display = ["id", "date", "customer", "total", "delivered_by"]
+    list_display_links = ["id", "date"]
+    list_filter = ["date", "customer", "delivered_by"]
+    search_fields = ["date", "customer", "total", "delivered_by"]
+    class Meta:
+        model = Order
+
+admin.site.register(Order, OrderAdminModel)
---
+++ foodie/models/order.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings # for ForeignKey on created_by
+from main.models import Employee
+from foodie.models import UserProfile
+
+#   #############################  ORDER  ######################################
+#
+class Order(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(
+            UserProfile,
+            on_delete=models.CASCADE,
+            default=1,
+    )
+    items_ordered = models.TextField(default="")
+    total = models.IntegerField()
+    delivered_by = models.ForeignKey(
+            Employee, 
+            on_delete=models.CASCADE,
+            limit_choices_to={'position' : 'DD'},
+            default=1,
+    )
+
+    def __str__(self):
+        return str(self.id) + " - " + self.customer.user.username
