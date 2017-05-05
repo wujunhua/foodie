67111c21807fac1918ddeb6b089a30e32323c02e
mike <mike@nighttype.com>
2017-05-05 02:06:52 -0400

    added more detailed views to the admin backend screen

--- foodie/admin.py
+++ foodie/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from .models import UserProfile
+from .models import Menu
 
 class UserProfileAdminModel(admin.ModelAdmin):
     model = UserProfile
     list_display = ['id', 'user',  'money', 'warnings_allowed', 'certified','money_spent', 'num_orders']
+    list_display_links = ["id", "user"]
     readonly_fields = ('user',)
     list_filter = ["certified"]
     search_fields = ['user__username']
-
+    class Meta:
+        model = UserProfile
 
 admin.site.register(UserProfile, UserProfileAdminModel)
+
+
+class MenuAdminModel(admin.ModelAdmin):
+    list_display = ["name", "short_description", "price", "rating", "created_by"]
+    list_display_links = ["name", "created_by"]
+    list_filter = ["name", "rating", "created_by"]
+
+    search_fields = ["name", "created_by"]
+    class Meta:
+        model = Menu
+
+admin.site.register(Menu, MenuAdminModel)
