b57d3f1b6b8ab6d4464eff657e9775f92b2e9033
Raymond Zhu <junhao.raymond.zhu@gmail.com>
2017-05-13 18:32:10 -0400

    Rename list_display methods in admin.

--- main/admin.py
+++ main/admin.py
@@ -4,22 +4,22 @@ from django.contrib import admin
 from .models import Employee
 
 class EmployeeModelAdmin(admin.ModelAdmin):
-    def get_user_active(self, obj):
+    def active(self, obj):
         if obj.user.is_active:
             return True
         else:
             return False
     get_user_active.boolean = True
 
-    list_display = ["id", "get_user_first_name", "get_user_last_name", "salary", "demotions_remaining", "rating", "get_user_active"]
+    list_display = ["id", "first_name", "last_name", "salary", "demotions_remaining", "rating", "active"]
     list_filter = ["rating", "user__first_name", "user__is_active"]
 
-    search_fields = ["id", "get_user_first_name", "get_user_last_name"]
+    search_fields = ["id", "first_name", "last_name"]
 
-    def get_user_first_name(self, obj):
+    def first_name(self, obj):
         return obj.user.first_name
 
-    def get_user_last_name(self, obj):
+    def last_name(self, obj):
         return obj.user.last_name
 
     class Meta:
