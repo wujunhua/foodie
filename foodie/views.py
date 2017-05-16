302412ceb52a8b9a7d94478d9b28723261a58a71
mike <mike@nighttype.com>
2017-05-16 05:36:43 -0400

    tiny edit

--- foodie/views.py
+++ foodie/views.py
@@ -98,9 +98,7 @@ def checkout(request):
 
 def deliveries(request):
     employee = Employee.objects.filter(user__id=request.user.id).first()
-    #user_profile = UserProfile.objects.filter(user__id=request.user.id).first()
     orders = Order.objects.filter(delivered_by=employee.id)
-    #orders = Order.objects.filter(customer_id=user_profile.id)
     return render(request, 'deliveries.html', {'nav_on': True, 'orders': orders})
 
 def delivery_detail(request, id=None):
